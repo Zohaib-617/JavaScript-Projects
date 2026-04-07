60185c1b2d11eb8526c4d1b93f17f50967bf6d82
Zohaib-617 <zk1209815@gmail.com>
2026-04-07 19:24:49 +0500

    displaying input value

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,14 +1,38 @@
 let btn = document.querySelector('.buttons')
 let input = document.querySelector('input')
+let resultoutput = document.querySelector('.result')
+let clear = document.querySelector('.clear')
 
 
-
+let count =0
 function yes(evt){
-    console.log(evt.target);
-    
-    if(evt.target.nodeName==='BUTTON'){
-        input.value = evt.target.innerText
+    let str = ''
+    if(evt.target.nodeName==='BUTTON' && evt.target.innerText!=='+' &&
+        evt.target.innerText!='-' &&
+        evt.target.innerText!='/' &&
+        evt.target.innerText!='*'){
+       str =  input.value.concat(evt.target.innerText) 
+        input.value = str
+             count++;
     }
-}
 
-btn.addEventListener('click',yes)
+    if(count>0){
+         if(evt.target.nodeName==='BUTTON' && evt.target.innerText=='+' ||
+        evt.target.innerText=='-' ||
+        evt.target.innerText=='/' ||
+        evt.target.innerText=='*' ){
+       str =  input.value.concat(evt.target.innerText) 
+       input.value = str
+             count++;
+             }
+            }
+        }
+
+
+
+
+btn.addEventListener('click',(evt)=>{
+     yes(evt)
+})
+
+
