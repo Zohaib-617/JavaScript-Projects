c3e7ba58937793ec5365feee6adaeea1e043300c
Zohaib-617 <zk1209815@gmail.com>
2026-04-07 22:46:56 +0500

    Fixed code

--- Calculator/script.js
+++ Calculator/script.js
@@ -3,31 +3,44 @@ let input = document.querySelector('input')
 let resultoutput = document.querySelector('.result')
 let clear = document.querySelector('.clear')
 
+ 
 
-let count =0
 function yes(evt){
-    let str = ''
-    if(evt.target.nodeName==='BUTTON' && evt.target.innerText!=='+' &&
-        evt.target.innerText!='-' &&
-        evt.target.innerText!='/' &&
-        evt.target.innerText!='*'){
-       str =  input.value.concat(evt.target.innerText) 
-        input.value = str
-             count++;
+
+  if(evt.target.innerText==='1'||
+     evt.target.innerText==='2'||
+     evt.target.innerText==='3'||
+     evt.target.innerText==='4'||
+     evt.target.innerText==='5'||
+     evt.target.innerText==='6'||
+     evt.target.innerText==='7'||
+     evt.target.innerText==='8'||
+     evt.target.innerText==='9'||
+     evt.target.innerText==='0'||
+     evt.target.innerText==='.'
+  ){
+     input.value =  input.value.concat(evt.target.innerText)
+  }
+
+  if(evt.target.innerText==='+'||
+     evt.target.innerText==='-'||
+     evt.target.innerText==='/'||
+     evt.target.innerText==='*'){
+        if(input.value.endsWith('1') ||
+           input.value.endsWith('2') ||
+           input.value.endsWith('3') ||
+           input.value.endsWith('4') ||
+           input.value.endsWith('5') ||
+           input.value.endsWith('6') ||
+           input.value.endsWith('7') ||
+           input.value.endsWith('8') ||
+           input.value.endsWith('9') ||
+           input.value.endsWith('0') ){
+            
+           input.value =  input.value.concat(evt.target.innerText)    
+      }
     }
-
-    if(count>0){
-         if(evt.target.nodeName==='BUTTON' && evt.target.innerText=='+' ||
-        evt.target.innerText=='-' ||
-        evt.target.innerText=='/' ||
-        evt.target.innerText=='*' ){
-       str =  input.value.concat(evt.target.innerText) 
-       input.value = str
-             count++;
-             }
-            }
-        }
-
+  }
 
 function finalvalue(){
     let resultvalue = eval(input.value)
@@ -35,18 +48,11 @@ function finalvalue(){
 }
 
 function clearoutput(){
-     str = '' 
-     input.value = str
-     count = 0
+     input.value = ''
 }
-
 
 clear.addEventListener('click',clearoutput)
 
 resultoutput.addEventListener('click',finalvalue)
 
-btn.addEventListener('click',(evt)=>{
-     yes(evt)
-})
-
-
+btn.addEventListener('click',yes)
