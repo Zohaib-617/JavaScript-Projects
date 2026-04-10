2a8d418911d267f48f32aa32d790ee911c8b1dab
Zohaib-617 <zk1209815@gmail.com>
2026-04-10 18:37:50 +0500

    weather app design

--- WeatherApp/script.js
+++ WeatherApp/script.js
@@ -1,11 +1,18 @@
- const url = fetch(' http://api.weatherapi.com/v1//current.json/documents:analyzeEntities?q=Pakistan&key=f63176229ff34962b06164427260904')
-.then((rawdata)=>{
+ let search = document.querySelector('.Search')
+ let input = document.querySelector('input')
+ let city = document.querySelector('.city')
+
+ function weather(){
+ fetch(' http://api.weatherapi.com/v1//current.json/documents:analyzeEntities?q=Pakistan&key=f63176229ff34962b06164427260904')
+ .then((rawdata)=>{
     return rawdata.json()
-})
-.then((data)=>{
-    console.log(data);  
-})
-.catch((err)=>{
-    console.log(err);
+ })
+ .then((data)=>{
+    console.log(data);
     
-})
+    
+ })
+ }
+weather();
+
+
