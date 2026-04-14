7a57ab70777755538b7a65e33a5454cedb3bba0b
Zohaib-617 <zk1209815@gmail.com>
2026-04-14 21:42:14 +0500

    fixing code

--- ColorFlipper/script.js
+++ ColorFlipper/script.js
@@ -4,7 +4,10 @@ let btn = document.querySelector('button')
 
 btn.addEventListener('click',()=>{
     let random = Math.random()
-let color = `rgb(${Math.floor(random*255)},${Math.floor(random*230)},${Math.floor(random*240)})`
+    let random2 = Math.random()
+    let random3 = Math.random()
+
+let color = `rgb(${Math.floor(random*255)},${Math.floor(random2*255)},${Math.floor(random3*255)})`
 
 div.style.backgroundColor = color;
 console.log(color);
