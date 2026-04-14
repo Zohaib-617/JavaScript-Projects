b24d6b9495b1f8b4ae577b50ad0ea01432ddd8e8
Zohaib-617 <zk1209815@gmail.com>
2026-04-14 21:06:57 +0500

    Functionality added

--- ColorFlipper/script.js
+++ ColorFlipper/script.js
@@ -1,4 +1,11 @@
-function colorflipper(){
-    console.log('making colorflipper project');
-    
-}
+let div = document.querySelector('div')
+let btn = document.querySelector('button')
+
+
+btn.addEventListener('click',()=>{
+    let random = Math.random()
+let color = `rgb(${Math.floor(random*255)},${Math.floor(random*230)},${Math.floor(random*240)})`
+
+div.style.backgroundColor = color;
+console.log(color);
+})
