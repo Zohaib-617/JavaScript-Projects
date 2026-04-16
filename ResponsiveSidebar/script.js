4624d195a5b67fff644136aa1cf24477eaf71296
Zohaib-617 <zk1209815@gmail.com>
2026-04-16 19:56:23 +0500

    Code done

---
+++ ResponsiveSidebar/script.js
@@ -0,0 +1,14 @@
+let innercontainer = document.querySelector('.container2')
+let menusymbol = document.querySelector('.ri-menu-line')
+let closesymbol = document.querySelector('.ri-close-fill')
+
+function display(){
+    innercontainer.classList.remove('display')
+}
+
+function remove(){
+     innercontainer.classList.add('display')
+}
+
+menusymbol.addEventListener('click',display)
+closesymbol.addEventListener('click',remove)
