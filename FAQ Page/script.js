f2c1f390d7b946441a9caa87b09f5f7111b8a807
Zohaib-617 <zk1209815@gmail.com>
2026-05-02 22:36:15 +0500

    done

--- FAQ Page/script.js
+++ FAQ Page/script.js
@@ -9,19 +9,22 @@ for(let h4 of h4s){
     h4.addEventListener('click',(evt)=>{
         const next = h4.nextElementSibling;
         const child = next.firstElementChild;
-       
+        var span = h4.firstElementChild
+    
         if( toggle=='appear'){
              console.log('hey');
             //  child.style.display = 'block'
              next.classList.add('open')
              next.classList.remove('answer')
              toggle = 'disappear'
+             span.textContent = '-'
         }
         else  {
             // child.style.display = 'none'
              next.classList.remove('open')
              next.classList.add('answer')
             toggle = 'appear'
+             span.textContent = '+'
             console.log('hey2');  
         }
         
