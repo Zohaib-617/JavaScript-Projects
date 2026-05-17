4398f7ec7c4e935f41e4b8074c8edeb6bd57698e
Zohaib-617 <zk1209815@gmail.com>
2026-05-17 18:30:54 +0500

    logic done

--- Memory Game/script.js
+++ Memory Game/script.js
@@ -6,13 +6,20 @@ let cardcontainer = document.querySelector('.card-container')
 let children = cardcontainer.children
 let frag = document.createDocumentFragment();
 
+let playerheading = document.querySelector('.player-heading')
+let pairheading = document.querySelector('.pair-heading')
+
 let content = '';
 let arr = [];
+let arr2 =[];
+let player1_pair = []
+let player2_pair = []
+let turn = 'player1'
 
 //showing cards at the beginning
 function showcards(){
+    startbtn.disabled = true
     for(let cardinner of cardinners){
-   
    setTimeout(()=>{
      cardinner.classList.add('card-inner-flip')
    },200)
@@ -27,26 +34,49 @@ function showcards(){
     cardcontainer.appendChild(frag)
 }
 
+function results(){
+    if(player1_pair.length+player2_pair.length ==6){
+        if(player1_pair.length>player2_pair.length){
+            playerheading.textContent = 'Player1 has won'
+        }
+       else if(player1_pair.length<player2_pair.length){
+            playerheading.textContent = 'Player2 has won'
+        }
+        else{
+            playerheading.textContent = 'Game Draw'
+        }
+    }
+}
+
 //logic for cards staying flipped if correct and unflip when wrong
 for(let cardinner of cardinners){
     cardinner.addEventListener('click',()=>{
         cardinner.classList.add('card-inner-flip')
         
-        arr.push(cardinner)
+       if(turn =='player1'){
+         arr.push(cardinner)
     
         if(content ==''){
             content = cardinner.textContent
         }
         else if(content ==cardinner.textContent){
             console.log('matched');
+            player1_pair.push('matched')
             content = '';
              arr.forEach((card)=>{
              card.style.cursor = "not-allowed";
-            
             })
+           arr = []
+           pairheading.textContent =`Matched Pairs: ${player1_pair.length}` 
         }
         else{
             console.log('wrong');
+            turn = 'player2'
+            playerheading.textContent = 'wrong' ;
+             setTimeout(()=>{
+             playerheading.textContent = turn ;
+             pairheading.textContent = `Matched Pairs: ${player2_pair.length}`
+             },1500)
             content = '';
            setTimeout(()=>{
             arr.forEach((card)=>{
@@ -56,12 +86,51 @@ for(let cardinner of cardinners){
             arr = [];
            },1200)
         }
-    })
+       }
+       
+       else{
+         arr2.push(cardinner)
+    
+        if(content ==''){
+            content = cardinner.textContent
+        }
+        else if(content ==cardinner.textContent){
+            console.log('matched');
+            player2_pair.push('matched')
+            content = '';
+             arr2.forEach((card)=>{
+             card.style.cursor = "not-allowed";
+            })
+           arr2 = []
+           pairheading.textContent = `Matched Pairs: ${player2_pair.length}`
+        }
+        else{
+            console.log('wrong');
+            turn = 'player1'
+             playerheading.textContent = 'wrong' ;
+             setTimeout(()=>{
+             playerheading.textContent = turn ;
+             pairheading.textContent = `Matched Pairs: ${player1_pair.length}`
+             },1500)
+            content = '';
+           setTimeout(()=>{
+            arr2.forEach((card)=>{
+            card.classList.remove('card-inner-flip')
+             card.style.cursor = "pointer";
+            })
+            arr2 = [];
+           },1200)
+        }
+       }
+       
+       results();
+ })
 }
 
 //reseting the game
-function resetgame(){
-    arr.forEach((card)=>{
+function resetgame(){ 
+    startbtn.disabled = false
+    cardinners.forEach((card)=>{
   card.classList.remove('card-inner-flip')
      card.style.cursor = "pointer";
     })
@@ -71,7 +140,7 @@ function resetgame(){
         frag.appendChild(children[Math.floor(Math.random() * children.length)])
     }
     cardcontainer.appendChild(frag)
-    showcards();
+  
 }
 resetbtn.addEventListener('click',resetgame)
 startbtn.addEventListener('click',showcards)
